11f6072cb4da3502e00dd818baa287730ad9635b
Wandré Nunes de Pinho Veloso <32296729+wandreveloso@users.noreply.github.com>
2022-04-11 08:34:30 -0300

    Update Say "Hello, World!" With Python.py

--- Say "Hello, World!" With Python.py
+++ Say "Hello, World!" With Python.py
@@ -3,6 +3,7 @@
 # Se você chegou a este código, entre no link e clique na estrela para apoiar esta iniciativa
 
 '''
+Desafio: https://www.hackerrank.com/challenges/py-hello-world
 O objetivo dessa questão é ensinar o uso do comando print, que no Python 3 necessita do parênteses para funcionar.
 O conteúdo dentro dos parênteses pode ser tanto um texto literal (aí usa-se aspas " ou apóstrofes ') ou conteúdo de variáveis (fora de aspas e apóstrofes).
 Diferentemente de outras linguagens como C e Java, somente 1 comando e 1 linha são necessários para fazer o clássico programa "Hello, World!"
