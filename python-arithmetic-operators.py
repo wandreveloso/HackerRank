84b2d255b6c404a20fb18d831dfce5b7f7a1786e
Wandré Nunes de Pinho Veloso <32296729+wandreveloso@users.noreply.github.com>
2022-04-11 09:05:40 -0300

    Create python-arithmetic-operators.py

---
+++ python-arithmetic-operators.py
@@ -0,0 +1,16 @@
+# Este é um comentário e não faz parte da resolução do problema
+# Este código foi retirado do GitHub https://github.com/wandreveloso
+# Se você chegou a este código, entre no link e clique na estrela para apoiar esta iniciativa
+
+'''
+Desafio: https://www.hackerrank.com/challenges/python-arithmetic-operators
+O objetivo dessa questão é ensinar o uso de operações aritméticas básicas e comando print, que no Python 3 necessita do parênteses para funcionar.
+Podemos criar novas variáveis para atribuir os resultados das operações de soma, subtração e multiplicação e, depois imprimir, ou já podemos imprimir o resultado, como fiz abaixo.
+Estamos trabalhando com operações simples, então, podemos imprimir diretamente o resultado das operações aritméticas
+'''
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
